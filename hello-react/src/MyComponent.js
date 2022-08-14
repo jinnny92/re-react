5086829e960d67f06b506d236535b8c76ac41774
jinnny92 <jinnny22@naver.com>
2022-08-14 19:49:34 +0900

    클래스형 컴포넌트의 state

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -7,17 +7,17 @@ class MyComponent extends Component {
   };
   static propTypes = {
     name: PropTypes.string,
-    favoriteName: PropTypes.number.isRequired,
+    favoriteNumber: PropTypes.number.isRequired,
   };
   render() {
-    const { name, favoriteName, children } = this.props;
+    const { name, favoriteNumber, children } = this.props;
     return (
       <div>
         안녕하세요, 제 이름은 {name}입니다. <br />
         children 값은 {children}
         입니다.
         <br />
-        제가 좋아하는 숫자는 {favoriteName}입니다.
+        제가 좋아하는 숫자는 {favoriteNumber}입니다.
       </div>
     );
   }
