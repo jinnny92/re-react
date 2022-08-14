3eb371ce3828f0109d5249970077387985560432
jinnny92 <jinnny22@naver.com>
2022-08-14 20:26:29 +0900

    클래스형 컴포넌트 state

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,22 +1,32 @@
 import { Component } from "react";
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    //state의 초기값 설정하기
-    this.state = {
-      number: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
+
   render() {
-    const { number } = this.state; //state를 조회할 때는 this.state로 조회
+    const { number, fixedNumber } = this.state; //state를 조회할 때는 this.state로 조회
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값 : {fixedNumber}</h2>
         <button //onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정
           onClick={() => {
-            //this.setState를 사용하여 state에 새로운 값을 넣을 수 있다
-            this.setState({ number: number + 1 });
-          }}></button>
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+            //위 코드와 아래 코드는 완전히 똑같은 기능을 한다
+            //아래 코드는 함수에서 바로 객체를 반환한다는 의미
+            this.setState((prevState) => ({
+              number: prevState.number + 1,
+            }));
+            console.log(this.state);
+          }}>
+          +1
+        </button>
       </div>
     );
   }
