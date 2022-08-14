605ac28ccef2b0dd3748aa2fa5b35bf49eb07bd7
jinnny92 <jinnny22@naver.com>
2022-08-14 19:46:09 +0900

    클래스형 컴포넌트의 state

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,22 +1,25 @@
-import React from "react";
+import { Component } from "react";
 import PropTypes from "prop-types";
 
-const MyComponent = ({ name, favoriteNumber, children }) => {
-  return (
-    <div>
-      안녕하세요, 제 이름은 {name}입니다. <br />
-      children 값은 {children}
-      입니다.
-    </div>
-  );
-};
-
-MyComponent.defaultProps = {
-  name: "기본 이름",
-};
-
-MyComponent.prototype = {
-  name: PropTypes.string,
-  favoriteNumber : PropTypes.number.isRequired
-};
+class MyComponent extends Component {
+  static defaultProps = {
+    name: "기본이름",
+  };
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteName: PropTypes.number.isRequired,
+  };
+  render() {
+    const { name, favoriteName, children } = this.props;
+    return (
+      <div>
+        안녕하세요, 제 이름은 {name}입니다. <br />
+        children 값은 {children}
+        입니다.
+        <br />
+        제가 좋아하는 숫자는 {favoriteName}입니다.
+      </div>
+    );
+  }
+}
 export default MyComponent;
