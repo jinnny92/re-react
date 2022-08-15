73636e32851f0a3c6ce6a467c82b014c31c34936
jinnny92 <jinnny22@naver.com>
2022-08-15 15:20:05 +0900

    ref : DOM에 이름 달기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,15 @@
-import React from "react";
-import EventPractice from "./EventPractice";
+import { Component } from "react";
+import ScrollBox from "./ScrollBox";
 
-const App = () => {
-  return <EventPractice />;
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>맨 밑으로</button>
+      </div>
+    );
+  }
+}
 
 export default App;
