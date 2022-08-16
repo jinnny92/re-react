c937e54866a53024cf2cd1ab5055d13eebd68412
yoojin <jinnny22@naver.com>
2022-08-16 15:04:40 +0900

    데이터 추가하기

---
+++ hello-react/src/IterationSample.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const IterationSample = () =>{
+    const [names, setNames] = useState([
+        {id : 1, text : '눈사람'},
+        {id : 2, text : '얼음'},
+        {id : 3, text : '눈'},
+        {id : 4, text : '바람'},
+
+    ]);
+
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
+
+    const onChange = e => setInputText(e.target.value);
+    const onClick = ()=>{
+        const nextNames = names.concat({
+            id : nextId, // nextId 값을 id로 설정하고
+            text : inputText
+        })
+        setNextId(nextId+1);
+        setNames(nextNames);
+        setInputText('');
+    }
+
+    const nameList = names.map(name => <li key ={name.id}>{name.text}</li>);
+    return(
+    <> 
+        <input value={inputText} onChange={onChange}/>
+        <button onClick={onClick}>추가</button>
+        <ul>{nameList}</ul>
+    </>
+    )
+}
+
+export default IterationSample;
