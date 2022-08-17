6e02ec301b1cf005e1ba525fe474ca6d7822f318
yoojin <jinnny22@naver.com>
2022-08-17 10:53:20 +0900

    useReducer

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,15 +1,10 @@
-import { useState } from 'react';
-import Info from './Info';
+import Counter from './Counter';
 
   const App = () =>{
-    const [visible, setVisible] = useState(false);
     return (
-    <div>
-        <button onClick={()=>{setVisible(!visible)}}>
-        {visible?'숨기기':'보이기'}
-        </button>
-    {visible && <Info/>}
-    </div>
+   
+    <Counter/>
+    
     )
   }
 
