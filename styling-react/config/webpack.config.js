f0351da936aa9cc3e73183bf7a1c2796f1fef2e2
yoojin <jinnny22@naver.com>
2022-08-18 16:13:28 +0900

    sass-loader

--- styling-react/config/webpack.config.js
+++ styling-react/config/webpack.config.js
@@ -507,18 +507,19 @@ module.exports = function (webpackEnv) {
             {
               test: sassRegex,
               exclude: sassModuleRegex,
-              use: getStyleLoaders(
-                {
-                  importLoaders: 3,
-                  sourceMap: isEnvProduction
-                    ? shouldUseSourceMap
-                    : isEnvDevelopment,
-                  modules: {
-                    mode: 'icss',
+              use: getStyleLoaders({
+                importLoaders: 2,
+                sourceMap: isEnvProduction && shouldUseSourceMap,
+              }).concat({
+                loader: require.resolve("sass-loader"),
+                options: {
+                  additionalData: `@import 'utils';`,
+                  sassOptions: {
+                    includePaths: [paths.appSrc + "/styles"],
+                    sourceMap: isEnvProduction && shouldUseSourceMap,
                   },
                 },
-                'sass-loader'
-              ),
+              }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
               // Remove this when webpack adds a warning or an error for this.
