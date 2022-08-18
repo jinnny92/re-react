ef7f2b0e903e50a778b030e35bb133003e84bc35
jinnny92 <jinnny22@naver.com>
2022-08-18 22:51:25 +0900

    컴포넌트스타일링

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,11 +1,11 @@
 import { Component } from "react";
-import SassComponent from "./SassComponent";
+import StyledComponent from "./StyledComponent";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        <StyledComponent />
       </div>
     );
   }
