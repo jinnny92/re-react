6807ae310a4a1ef029cc4a21c2eaf7e3f7e715c3
jinnny22 <jinnny22@naver.com>
2022-09-05 16:02:16 +0900

    thunk를 이용한 비동기처리

---
+++ learn-redux-middleware/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CounterContainer from './containers/CounterContainer';
+import SampleContainer from './containers/SampleContainer';
+
+const App = ()=>{
+  return(
+    <div>
+      <SampleContainer/>
+    </div>
+  )
+}
+
+export default App;
---
+++ learn-redux-middleware/src/containers/SampleContainer.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { connect } from "react-redux";
+import Sample from "../components/Sample";
+
+import { getPost, getUsers } from '../modules/sample';
+
+const { useEffect } = React;
+const SampleContainer = ({
+  getPost,
+  getUsers,
+  post,
+  users,
+  loadingPost,
+  loadingUsers
+}) => {
+  // 클래스 형태 컴포넌트였다면 componentDidMount
+  useEffect(() => {
+    // useEffect에 파라미터로 넣는 함수는 async로 할 수 없기 때문에
+    // 그 내부에서 async 함수를 선언하고 호출해 줍니다.
+    const fn = async () => {
+      try {
+        await getPost(1);
+        await getUsers(1);
+      } catch (e) {
+        console.log(e); // 에러 조회
+      }
+    };
+    fn();
+  }, [getPost, getUsers]);
+
+};
+
+
+
+export default connect(
+  ({ sample, loading }) => ({
+    post: sample.post,
+    users: sample.users,
+    loadingPost: loading.GET_POST,
+    loadingUsers: loading.GET_USERS
+  }),
+  {
+    getPost,
+    getUsers
+  }
+)(SampleContainer);
