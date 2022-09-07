888082d000146879c8b683d554e693df7ef2da2f
jinnny92 <jinnny22@naver.com>
2022-09-07 10:47:40 +0900

    컴퓨터 랜덤 아이템 선택

--- rookpaperscissor/src/App.js
+++ rookpaperscissor/src/App.js
@@ -26,14 +26,30 @@ const choice = {
 function App() {
   const [userSelect, setUserSelect] = useState(null);
 
+  const [computerSelect, setComputerSelect] = useState(null);
+
   const play = (userChoice) => {
     setUserSelect(choice[userChoice]);
+    //유저가 선택할때(버튼을 누를때), 컴퓨터가 랜덤한 값을 보여준다
+    let computerChoice = randomChoice(); //컴퓨터가 랜덤하게 값을 선택한다는 함수를 만들어서 결과값을 Computerchoice라는 변수에 넣어준다
+    setComputerSelect(computerChoice);
   };
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice); //객체에 있는 key값만 뽑아서 배열로 만들어주는 함수
+    console.log("Item array", itemArray);
+    //컴퓨터에서 랜덤으로 값을 줄 수 있는 함수 :  Math.random()
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    //console.log(randomItem); //random이라는 함수는 0~1사이의 값을 랜덤하게 반환하는데 우리가 가져올 수 있는건 숫자인데 위의 choice랑 어떻게 연관을 지을 수 있는지
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
+
   return (
     <div>
       <div className="main">
         <Box name="You" item={userSelect} />
-        {/* <Box name="Computer" /> */}
+        <Box name="Computer" item={computerSelect} />
       </div>
       <div className="main">
         <button onClick={() => play("scissors")}>가위</button>
