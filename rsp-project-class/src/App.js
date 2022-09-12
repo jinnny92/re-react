584234ec69d8fdcfb6950123d5d78e879d8ab57d
jinnny92 <jinnny22@naver.com>
2022-09-12 12:31:54 +0900

    가위바위보 클래스형컴포넌트로 만들기

---
+++ rsp-project-class/src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import "./App.css";
+import BoxClass from "./component/BoxClass";
+
+const choice = {
+  rock: {
+    name: "rock",
+    img: "https://blog.kakaocdn.net/dn/pSJwo/btqXJV1lACE/nx5XrxkCLWXh9UsnoS8vbK/img.png",
+  },
+  scissors: {
+    name: "scissors",
+    img: "https://blog.kakaocdn.net/dn/HfURw/btqXKvOTNWK/gWTwPXEg9QzSV0ilOuwuak/img.png",
+  },
+  paper: {
+    name: "paper",
+    img: "https://blog.kakaocdn.net/dn/bmjB2s/btqXHhp6kpG/TH14W4U612SxKo9uuR2sB0/img.png",
+  },
+};
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userSelect: null,
+      computerSelect: null,
+      result: "",
+    };
+  }
+  play = (userChoice) => {
+    let computerChoice = this.computerChoice();
+    this.setState({
+      userSelect: choice[userChoice],
+      computerSelect: computerChoice,
+      result: this.judgement(choice[userChoice], computerChoice),
+    });
+  };
+
+  judgement = (user, computer) => {
+    if (user.name === computer.name) {
+      return "tie";
+    } else if (user.name === "rock")
+      return computer.name === "scissors" ? "win" : "lose";
+    else if (user.name === "scissors")
+      return computer.name === "paper" ? "win" : "lose";
+    else if (user.name === "paper")
+      return computer.name === "rock" ? "win" : "lose";
+  };
+  computerChoice = () => {
+    let itemArray = Object.keys(choice);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
+  render() {
+    return (
+      <div>
+        <div className="main">
+          <BoxClass
+            title="You"
+            item={this.state.userSelect}
+            result={this.state.result}
+          />
+          <BoxClass
+            title="Computer"
+            item={this.state.computerSelect}
+            result={this.state.result}
+          />
+        </div>
+        <div className="main">
+          <button onClick={() => this.play("scissors")}>가위</button>
+          <button onClick={() => this.play("rock")}>바위</button>
+          <button onClick={() => this.play("paper")}>보</button>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ rsp-project-class/src/component/BoxClass.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export default class BoxClass extends Component {
+  constructor() {
+    super();
+    this.result = "";
+  }
+  getResult = () => {
+    if (
+      this.props.title === "Computer" &&
+      this.props.result !== "tie" &&
+      this.props.result !== ""
+    ) {
+      this.result = this.props.result === "win" ? "lose" : "win";
+    } else {
+      this.result = this.props.result;
+    }
+  };
+  render() {
+    this.getResult();
+    return (
+      <div className={`box ${this.result}`}>
+        <h1>{this.props.title}</h1>
+        <img className="item-img" src={this.props.item?.img} />
+        <h3>{this.result}</h3>
+      </div>
+    );
+  }
+}
