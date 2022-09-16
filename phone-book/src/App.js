7211a20ae1eb5a6c729d1242e170af81e582f83d
jinnny92 <jinnny22@naver.com>
2022-09-17 00:40:12 +0900

    연락처프로젝트(redux버전)생성

---
+++ phone-book/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+//1. 왼쪽에는 연락처 등록하는 폼이, 오른쪽에는 연락처 리스트와 search창이 있다
+//2. 리스트에 유저 이름과 전화번호를 추가할 수 있다.
+//3. 리스트에 아이템이 몇 개 있는지 보인다
+//4. 사용자가 유저를 이름검색으로 찾을 수 있다
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
